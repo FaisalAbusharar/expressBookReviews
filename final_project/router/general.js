bb8c4987e52448e18c60ba9050bed0f939883cd8
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2024-07-24 20:33:15 +0300

    TASK 5, reviews.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,8 +62,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let booksList = Object.values(books)
+  let book = booksList.find(b => b.isbn===isbn);
+  if (book) {
+    const reviews = book.reviews;
+    res.send(reviews)
+  } else {
+    res.send("No book found for ISBN " + isbn)
+  }
 });
 
 module.exports.general = public_users;
