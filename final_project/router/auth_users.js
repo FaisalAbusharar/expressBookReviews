5b253773eebd5459de4a9575efd439d261f8b346
Faisal Abusharar <77110462+FaisalAbusharar@users.noreply.github.com>
2024-07-24 21:38:19 +0300

    TASK 7, login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,49 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  {id: 1, username: 'Faisal', password: 'pwd123'}
+];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+  return users.find(user=>user.username===username && user.password === password)
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+
+  let usersList = Object.values(users);
+  let user = usersList.find(b => b.username==username)
+ if (user) {
+
+   if (users.password === password) {
+
+     return true;
+   }
+ }
+
+ return false;
 }
 
-//only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ const { username, password } = req.body;
+
+
+ if (!username || !password) {
+   return res.status(400).json({ message: 'Please provide a valid username and password' });
+ }
+ const user = users.find(u => u.username === username && u.password === password);
+
+
+ if (username === user.username && password === user.password) {
+   const accessToken = jwt.sign({ username, userPassword: password }, "secretKey", { expiresIn: '1h' });
+
+
+   req.session.accessToken = accessToken;
+
+   return res.status(200).json({ message: 'Login successful',accessToken });
+ } else {
+   return res.status(401).json({ message: 'Invalid username or password' });
+ }
 });
 
 // Add a book review
